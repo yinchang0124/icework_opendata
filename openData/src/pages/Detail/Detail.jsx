d041dec867728ccc2361166ee753057b5b337511
yinchang0124 <1363720314@qq.com>
2018-11-26 10:09:32 +0800

    opendata

---
+++ openData/src/pages/Detail/Detail.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import LatestActivity from './components/LatestActivity';
+import PostCategory from './components/PostCategory/PostCategory';
+
+export default class Detail extends Component {
+  static displayName = 'Detail';
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="detail-page">
+        <LatestActivity />
+      </div>
+    );
+  }
+}
---
+++ openData/src/pages/Detail/components/LatestActivity/LatestActivity.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Card from './Card';
+//import BarChart from './BarChart';
+import PostCategory from './PostCategory';
+
+const getData = () => {
+  return Array.from({ length: 10 }).map((item, index) => {
+    return {
+      name: `${index + 1}. 造物节`,
+      num: parseInt(Math.random() * 1000),
+    };
+  });
+};
+
+export default class LatestActivity extends Component {
+  static displayName = 'LatestActivity';
+
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  constructor(props){
+    super(props);
+    this.state={
+      date:'',
+      name:''
+    };
+  }
+
+  getData(){
+
+  }
+
+  render() {
+    const dataSource = getData();
+    const columns = [
+      {
+        title: '时间',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '购买者',
+        dataIndex: 'num',
+        key: 'num',
+      },
+    ];
+
+    return (
+      <Card
+        title="详细信息"
+        subTitle="最近交易"
+        // summary={[
+        //   { label: '本周发布活动数', value: '123' },
+        //   { label: '上周发布活动数', value: '349' },
+        //   { label: '累计发布活动数', value: '23,239' },
+        // ]}
+        link={{ text: '发布活动明细', href: '#' }}
+        dataSource={dataSource}
+        columns={columns}
+        //content={<BarChart />}
+        content = {<PostCategory />}
+      />
+    );
+  }
+}
